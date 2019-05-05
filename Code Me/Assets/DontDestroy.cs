00ce0d0953ac627550d86a4360a266b2bbc1ab4e
WilliamBrownCoder <William.Brown.Coder@gmail.com>
2019-05-05 07:45:33 -0400

    Final build and bug fixes

--- Code Me/Assets/DontDestroy.cs
+++ Code Me/Assets/DontDestroy.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    private static DontDestroy _instance;
+
     private void Awake()
     {
+        //if we don't have an [_instance] set yet
+        if (!_instance)
+            _instance = this;
+        //otherwise, if we do, kill this thing
+        else
+            Destroy(this.gameObject);
+
         DontDestroyOnLoad(gameObject);
     }
 }
